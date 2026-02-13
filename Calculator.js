ce02a9b7e4c865b7d9e0dd1c22b1d30a6352ed38
Ashtin A. <antoashtin@gmail.com>
2026-02-12 20:35:53 -0500

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,68 @@
+console.log(Math.PI);
+console.log(Math.E);
+
+let x = 10;
+let y = 23434;
+let z = 4;
+
+
+//z = Math.floor(x) ;
+//z = Math.ceil(y)  ;
+//z = Math.round(x) ;
+//z = Math.trunc(y) ; 
+//z = Math.pow(x , y) ;
+//z = Math.sqrt(x);
+//z = Math.log(x);
+//z = Math.sin(x);
+//z = Math.cos(x);
+//z = Math.tan(x);
+//z = Math.abs(x);
+//z = Math.sign(x);
+
+//let max = Math.max(x, y, z);
+//let min = Math.min(x, y, z);
+
+//console.log(z);
+
+//const max = 20;
+//const min = 1;
+
+//let randomando = Math.floor(Math.random () * (max)) + min; 
+//console.log(randomando);
+
+
+/*
+const mybutton= document.getElementById("mybutton");
+const mylabel = document.getElementById("mylabel");
+const mylabel2 = document.getElementById("mylabel2");
+const mylabel3 = document.getElementById("mylabel3");
+let random;
+let random2;
+let random3;
+
+mybutton.onclick = function(){
+random = Math.floor(Math.random() * max) + min; 
+random2 = Math.floor(Math.random() * max) + min; 
+random3 = Math.floor(Math.random() * max) + min; 
+mylabel.textContent = random;
+mylabel2.textContent = random2;
+mylabel3.textContent = random3;
+}
+
+const min= 1;
+const max= 6;
+*/
+let a = 38;
+let b = "38";
+
+
+
+//console.log(a < b);
+//console.log(a > b);
+//console.log(a >= b);
+//console.log(a <= b);
+//console.log(a = b);
+//console.log(a == b);
+//console.log(a === b);
+//console.log(a !== b);
+//console.log(a != b);
